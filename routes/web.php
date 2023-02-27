c48d39bd5e7c0d7f43a5a9c35628ab2e2333d0e1
diba <info.dibaalizai@gmail.com>
2023-02-27 09:54:45 +0100

    Practise Section 1

--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,22 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index'])->name('home');
+// Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('posts/{post:slug}', [PostController::class, 'show']);
-Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+// Route::get('posts/{post:slug}', [PostController::class, 'show']);
+// Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
-Route::post('newsletter', NewsletterController::class);
+// Route::post('newsletter', NewsletterController::class);
 
-Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
-Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
+// Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+// Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
-Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+// Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+// Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
-Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+// Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
-// Admin Section
-Route::middleware('can:admin')->group(function () {
-    Route::resource('admin/posts', AdminPostController::class)->except('show');
-});
+// // Admin Section
+// Route::middleware('can:admin')->group(function () {
+//     Route::resource('admin/posts', AdminPostController::class)->except('show');
+// });
